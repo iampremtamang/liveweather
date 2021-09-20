fa82396e147aa01ed8869753e49006d2e35e17ff
premds <ktamangprem@gmail.com>
2021-09-20 08:47:48 +0545

    weather app works

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -31,75 +31,11 @@ class _SearchPageState extends State<SearchPage> {
             width: 300,
           ),
         ),
-        Container(
-          padding: EdgeInsets.only(left:32, right:32),
-          child: Column(
-            children: <Widget>[
-              Text(
-                'Live Weather',
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color:Colors.white70,
-                  ),
-                ),
-              Text(
-                "Search weather by location",
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.w200,
-                  color:Colors.white70,
-
-                ),
-
-              ),
-              SizedBox(height: 24,),
-              TextFormField(
-                controller: cityController,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.search, color:Colors.white70),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    borderSide: BorderSide(
-                      color:Colors.white70,
-                      style: BorderStyle.solid,
-                    )
-                    
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      borderSide: BorderSide(
-                        color:Colors.blue,
-                        style:BorderStyle.solid
-
-                      ),
-                    ),
-                    hintText: "City Name",
-                    hintStyle: TextStyle(color:Colors.white70),
-                  
-              
-                ),
-                style:TextStyle(color:Colors.white70),
-              ),
-              SizedBox(height:20,),
-              Container(
-                width: double.infinity,
-                height: 50,
-                child: FlatButton(
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
-                  onPressed: () {
-
-                  },
-                  color: Colors.lightBlue,
-                  child:Text('Search', style: TextStyle(color:Colors.white70,fontSize: 16),),
-                ),
-              )
-            ],
-          ),
-        ),
+  
         BlocBuilder<WeatherBloc, WeatherState>(
           builder: (context,state) {
-            if(state is WeatherIsNotLoaded) {
+            
+            if(state is WeatherIsNotLoaded || state is WeatherIsNotSearched) {
               return Container(
                 padding: EdgeInsets.only(left:32, right:32),
                 child: Column(
@@ -176,7 +112,7 @@ class _SearchPageState extends State<SearchPage> {
             } else if (state is WeatherIsLoaded) {
                 return WeatherDetail(state.getWeather, cityController.text);
             }               else {
-              return Text('error');
+              return Text('error', style:TextStyle(color: Colors.white, fontSize: 20));
               }
             }
            
